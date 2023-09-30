92fa7e1edb2525511a9ec0a382696b9f3ee9ebd0
Gabriele La Piana <Gabrilp@live.com>
2023-09-30 12:38:04 +0200

    Initial deployment

---
+++ src/js/main.js
@@ -0,0 +1,106 @@
+import {get} from 'lodash';
+import {requestCity, requestGeolocation, apiKey, lang} from './request.js'
+
+
+const searchForm = document.querySelector('.search-location');
+const cityValue = document.querySelector('.search-location input');
+const cityName = document.querySelector('.city-name p');
+const cardBody = document.querySelector('.card-body');
+const displayCard = document.querySelector('.back-card');
+const errorMessage = document.getElementById('error-message');
+
+const kelvinToCelsius = (kelvin) => Math.round(kelvin - 273.15);
+
+function updateWeatherApp(cityData) {
+    // Safely access properties using _.get() from lodash library
+    const weatherDescription = get(cityData, 'weather[0].description', '');
+    const icon = get(cityData, 'weather[0].icon', '');
+    const iconSrc = `./images/${icon}.png`;
+    const temp = kelvinToCelsius(get(cityData, 'main.temp', 0));
+    const tempMax = kelvinToCelsius(get(cityData, 'main.temp_max', 0));
+    const tempMin = kelvinToCelsius(get(cityData, 'main.temp_min', 0));
+    const feelsLike = kelvinToCelsius(get(cityData, 'main.feels_like', 0));
+    const humidity = get(cityData, 'main.humidity', 0);
+
+    cardBody.innerHTML = `
+        <div class="card-mid row">
+            <div class="col-6 text-center temp">
+                <span>${temp}&deg;C</span>
+            </div>
+
+            <div class="col-3 condition-temp">    
+                <p class="condition">${weatherDescription}</p>
+                <p class="high">&#8679; ${tempMax}&deg;C</p>
+                <p class="low">&#8681; ${tempMin}&deg;C</p>
+            </div>
+
+            <div class="icon-temp col-3">
+                <div class="icon-container card shadow mx-auto">
+                    <img src="${iconSrc}" class="weather-icon">
+                </div>
+            </div>
+        </div>
+    <div id='bottom-wrap'>
+        <div class="card-bottom px-5 py-4 row">
+            <div class="col text-center">
+                <p>${feelsLike}&deg;C</p>
+                <span>Temperatura percepita</span>
+            </div>
+        </div>
+
+        <div class="card-bottom px-5 py-4 row">
+            <div class="col text-center">
+                <p>${humidity}%</p>
+                <span>Umidità</span>
+            </div>
+        </div>
+    </div>
+    `;
+
+    cityName.textContent = cityData.name;
+    displayCard.classList.remove('d-none');
+};
+
+
+searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    try {
+        const citySearched = cityValue.value.trim();
+        searchForm.reset();
+        
+        const data = await requestCity(citySearched);
+        
+        updateWeatherApp(data);
+        
+            errorMessage.textContent = '';
+            errorMessage.classList.add('d-none');
+
+        displayCard.classList.remove('animate-slide-in');
+
+        setTimeout(() => {
+            displayCard.classList.add('animate-slide-in');
+        }, 10);
+
+    } catch (error) {
+        console.error('Error fetching city data:', error);
+        errorMessage.textContent = 'An error occurred. Please check your search query and try again.';
+        errorMessage.classList.remove('d-none');
+    }
+});
+
+window.addEventListener('load', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const lat = get(position, 'coords.latitude', 0);
+            const lon = get(position, 'coords.longitude', 0);
+
+            try {
+                const data = await requestGeolocation(lat, lon);
+                updateWeatherApp(data);
+            } catch (error) {
+                console.error('Error fetching geolocation data:', error);
+            }
+        });
+    }
+});
---
+++ src/js/request.js
@@ -0,0 +1,49 @@
+//const { config } = require('dotenv');
+import axios from "axios";
+import {get} from "lodash";
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
+const lang = 'it';
+//config();
+
+let apiKey;
+
+if (typeof window === 'undefined' || typeof window.process === 'undefined') {
+  apiKey = process.env.API_KEY;
+} else {
+    apiKey = 'undefined';
+}
+
+export async function requestCity(cityData) {
+    const query = `?q=${cityData}&appid=${apiKey}&lang=${lang}`;
+
+    axios.get(baseURL + query)
+    try {
+        const response = await axios.get(baseURL + query);
+    
+        if (response.status === 200) {
+          // Using Lodash to safely access properties in the JSON response
+          const jsonData = get(response, 'data', {});
+          return jsonData;
+        } else {
+          console.error('Non-OK response status:', response.status);
+        }
+      } catch (error) {
+        console.error('There was a problem with the request:', error);
+      }
+    }
+
+export async function requestGeolocation(lat, lon) {
+    const queryLocation = `?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=${lang}`;
+
+    try {
+        const response = await axios.get(baseURL + queryLocation);
+        if (response.data.cod === 200) {
+            const jsonData = get(response, 'data', {});
+            return jsonData;
+        } else {
+            console.error('Non-ok response status', response.status);
+        }
+    } catch (error) {
+        console.error('There was a problem with the request:', error);
+    }
+}
